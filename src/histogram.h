6a20bb78e8fdcbe39de2783e879a016e1f251cc6
jdpetticrew <jdpetticrew1@sheffield.ac.uk>
2017-11-14 21:50:11 +0000

    Binner is Working

--- src/histogram.h
+++ src/histogram.h
@@ -4,6 +4,15 @@
 class histogram{
 	private:
 		double* dataset;
+		int size;
+		double max;
+		double min;
+		void max_min();
+		double binsize;
+		void binner();
+		double* binEdges;
+		double* binCenters;
+		double* binValues;
 	public:
 		histogram(double* data, int size);
 		~histogram();
--- src/histogram_class.cpp
+++ src/histogram_class.cpp
@@ -2,17 +2,63 @@
 #include <stdio.h>
 
 //constructor for histogram class
-histogram::histogram(double* data, int size){
+histogram::histogram(double* data, int size):size(size){
 	dataset = new double[size];
 	int i;
 	for(i=0;i<size;i++){
 		dataset[i]=data[i];
 	}
-	printf("I have an array of %d elements\n",i);
+	max_min();
+	binner();
 };
 
 //destructor for histogram class
 histogram::~histogram(){
 	delete[] dataset;
+	delete[] binEdges;
+	delete[] binCenters;
+	delete[] binValues;
 };
 
+//finds maximum and minimum in dataset
+void histogram::max_min(){
+	max=dataset[0];
+	min=dataset[0];
+	int i;
+	for(i=1;i<size;i++){
+		if(dataset[i]>max) max=dataset[i];
+		else if(dataset[i]<min) min=dataset[i];
+	}
+};
+
+//Bins the DataSet
+void histogram::binner(){
+	printf("Enter Bin Width:");
+	scanf("%lf",&binsize);
+	int bins=(max-min)/binsize+3;
+	binEdges = new double[bins+1];
+	binCenters = new double[bins];
+	binValues = new double[bins];
+	int i, j;
+	for(i=0;i<(bins+1);i++){
+		binEdges[i]=(min-binsize)+i*binsize;
+	}
+	for(i=0;i<bins;i++){
+		binCenters[i]=(binEdges[i+1]-binEdges[i])/2 +binEdges[i];
+		binValues[i]=0;
+	}
+	
+	for(j=0;j<size;j++){
+		for(i=0;i<bins;i++){
+			if(dataset[j]>=binEdges[i] && dataset[j]<binEdges[i+1]){
+				binValues[i]++;
+			}
+		}
+	}
+	FILE *binout;
+	binout=fopen("Hist.txt","w");
+	for(i=0;i<bins;i++){
+		fprintf(binout,"%lf, %lf\n",binCenters[i], binValues[i]);
+	}
+	fclose(binout);
+};
