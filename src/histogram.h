31683c591e2ae3241cd17f64b0625164b57772a3
jdpetticrew <jdpetticrew1@sheffield.ac.uk>
2017-11-16 13:54:42 +0000

    Improved Commenting and added some Gets

--- src/histogram.h
+++ src/histogram.h
@@ -6,10 +6,8 @@ class histogram{
 		double* dataset;
 		int size;
 		double max;
-		double min;
-		void max_min();
+		double min;		
 		double binsize;
-		void binner();
 		double* binEdges;
 		double* binCenters;
 		double* binValues;
@@ -20,19 +18,25 @@ class histogram{
 		double a;
 		double b;
 		double c;
-		void fit();
 		double FWHM;
 		char* filename;
-		void print();
-		void print_custom();
 		int bins;
 		int charstored=0;
+		
+		void binner(); //Bins the DataSet and calculates mean and standard deviation
+		void fit(); //Fits Gaussian Probability Density Function in the form f(x) = a exp(-((x-b)/c)^2)
+		void max_min(); //finds maximum and minimum in dataset
+		void print(); //Prints Histogram Data to Hist.txt Used if no string is passed in constructor
+		void print_custom(); //Prints Histogram Data to custom file
 	public:
-		histogram(double* data, int size);
-		histogram(double* data, int size, double binsize);
-		histogram(double* data, int size, char* fname);
-		histogram(double* data, int size, double binsize, char* fname);
+		histogram(double* data, int size); //constructor for histogram class Ask for Bin Size
+		histogram(double* data, int size, double binsize); //constructor for histogram class passed Bin Size
+		histogram(double* data, int size, char* fname); //constructor for histogram class Ask for Bin Size and passes string for printing Hist to file.
+		histogram(double* data, int size, double binsize, char* fname); //constructor for histogram class passed Bin Size and passes string for printing Hist to file.
 		~histogram();
-		double Get_FWHM(){return FWHM;};
+		double Get_FWHM(){return FWHM;}; //Returns Full-Width @ Half-Maximum
+		double Get_Mean(){return mean;}; //Returns Mean of Histogram
+		double Get_SDev(){return sdev;}; //Returns Standard Deviation of Histogram
+		void show_fit();// displays Probability Density function of gaussian
 };
 #endif
